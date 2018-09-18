1c73eb3a577b14ddde40d9318a380ba2ac268caa
jbae11 <jbae11@illinois.edu>
2018-09-17 20:11:26 -0500

    old saltproc converter script

--- script/old_saltproc_converter.py
+++ script/old_saltproc_converter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 from pyne import nucname
+from pyne.material import Material
 
 f = h5py.File('./db_saltproc_40years.hdf5', 'r')
 
@@ -29,9 +30,58 @@ iso_names = np.array(iso_names)
 shape = core_adensity_after.shape
 
 
-# way to convert adensity to mass
+def adens_to_mass(fuel_vol, array, iso_names):
+    shape = array.shape
+    mass_array = np.zeros(shape)
+    for num, row in enumerate(array):
+        mat_dict = {}
+        mass_comp = np.zeros(len(row))
+        for indx, val in enumerate(row):
+            mat_dict[iso_names[indx]] = val
+        mat = Material()
+        mat.from_atom_frac(mat_dict)
+        # mat.comp is mass density
+        for key, val in mat.comp.items():
+            key = nucname.name(key)
+            iso_indx = list(iso_names).index(key)
+            # mass = mass density * volume * unit(barns)
+            mass_comp[indx] = val * fuel_vol * 1e24
+        mass_array[num] = mass_comp
+
+        # you still there?
+        if num == (len(array) // 2):
+            print('halfway there')
+    return mass_array
 
 
+# get initial composition of msbr:
+mat = Material()
+mat_dict = {'Li7': 71.85, 
+            'Be9': 16,
+            'Th232': 12,
+            'U233': 0.25,
+            'F19': 71.85+16*2+12*4+0.25*4}
+mat.from_atom_frac(mat_dict)
+init_mass_comp = mat.comp
+init_mass_comp_array = np.zeros(len(iso_names))
+for key,  val in init_mass_comp.items():
+    key = nucname.name(key)
+    indx = list(iso_names).index(key)
+    init_mass_comp_array[indx] = val
+
+# msbr [cm3]
+fuel_vol = 48.7e6
+# msbr [g/cm3]
+mass_dens = 3.35
+
+core_mass_after = adens_to_mass(fuel_vol, core_adensity_after,
+                                iso_names)
+print('PROGRESS?')
+core_mass_before = adens_to_mass(fuel_vol, core_adensity_before,
+                                iso_names)
+print('PROGRESS?')
+th_mass = adens_to_mass(fuel_vol, th_tank_adensity, iso_names)
+noble_mass = adens_to_mass(fuel_vol, noble_adensity, iso_names)                                
 
 # write
 k = h5py.File('new_msbr.hdf5', 'w')
@@ -39,9 +89,9 @@ k.create_dataset('blanket composition after reproc', data=np.zeros(shape))
 k.create_dataset('blanket composition before reproc', data=np.zeros(shape))
 k.create_dataset('blanket refill tank composition', data=np.zeros(shape))
 
-k.create_dataset('driver composition before reproc', data=core_adensity_before)
-k.create_dataset('driver composition after reproc', data=core_adensity_after)
-k.create_dataset('driver refill tank composition', data=th_tank_adensity)
+k.create_dataset('driver composition before reproc', data=core_mass_before)
+k.create_dataset('driver composition after reproc', data=core_mass_after)
+k.create_dataset('driver refill tank composition', data=th_mass)
 
 k.create_database('fissile tank composition', data=np.zeros(shape))
 
@@ -51,10 +101,10 @@ k.create_database('iso zai', data=iso_zai)
 k.create_database('keff_BOC', data=keff_boc)
 k.create_database('keff_EOC', data=keff_eoc)
 
-k.create_database('siminfo_driver_init_comp', data= )
-k.create_database('siminfo_driver_mass_density', data=)
+k.create_database('siminfo_driver_init_comp', data= init_mass_comp_array)
+k.create_database('siminfo_driver_mass_density', data=3.35)
 
 k.create_database('siminfo_timestep', data=3)
 k.create_database('siminfo_totstps', data=int(365*40/3))
 
-k.create_database('waste tank composition', data=noble_adensity)
+k.create_database('waste tank composition', data=noble_mass)
